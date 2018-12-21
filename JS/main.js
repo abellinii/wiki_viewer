0b9d6a8ecd51d41e8c56f7b436cd748a2d37748d
abellinii <wadeabel12@hormail.com>
2018-12-20 22:20:22 -0800

    first

--- JS/main.js
+++ JS/main.js
@@ -1,178 +1,127 @@
 $(document).ready(function() {
-  var urlLink = "";
+  console.log('we in')
+  var searchTerm = "";
+  var page = [];
+  var wiki = "https://en.wikipedia.org/wiki/";
   var url = "";
-  var url2 = "";
-  page = [];
-  var k = 0;
-  var kblock = "";
-  var json = {};
-  var lastChannel="";
- 
-  
-    var twitch = {
-    width: 0,
-    height: 0,
-    channel: ""
-  };
+  var urlLink = "";
+  var wikiPage = [];
 
+  //Hide once page is loaded
+  $("#results").hide();
 
+  //call JSON with the search term
+  function ajax() {
+    $.getJSON(url, function(data) {
 
-  var resizeTimer;
-
-$(window).on('resize', function(e) {
-
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(function() {
-
-    
- 
- if(window.innerWidth <600){
- twitch.width = window.innerWidth ;
-   twitch.height= window.innerHeight  ;
-}else{
-    twitch.width = window.innerWidth * 0.77;
-    twitch.height= window.innerHeight * 0.77 ;
-}
-
-$('#linked-page').empty();
-openTwitch(); 
-  }, 250);
-
-});
-
-
-
-
-  //List of Twitch accounts to pull data from.
-  var twitchAccounts = [
-    "ESL_SC2",
-    "OgamingSC2",
-    "cretetion",
-    "freecodecamp",
-    "storbeck",
-    "habathcx",
-    "RobotCaleb",
-    "noobs2ninjas"
-  ];
-  //Iterate through twitch accounts and create a url to get image/name and a url to check the streaming status.
-  twitchAccounts.forEach(function(account) {
-    
-    //image/name url.
-    url =
-      "https://wind-bow.gomix.me/twitch-api/channels/" +
-      account +
-      "?callback=?";
-
-    //Streaming url.
-    url2 = "https://wind-bow.glitch.me/twitch-api/streams/" + account;
-
-    // object to manipulate the data
-    var json = {
-      //JSON call to find out if the channel is currently streaming.
-      addProp: $.getJSON(url2, function(data) {}),
-
-      //JSON call to create html content and append it to page.
-      buildBlock: $.getJSON(url, function(data) {
-        console.log();
-        // Create a div containing the channel info.
-        $("#blocks").append(
-          '<div class ="block" id ="block' +
-            k +
-            '"><img class="image" src=' +
-            data.logo +
-            ' >' +
-            '<p class="name">' +
-            data.name.toUpperCase() +
-            "</p></div><br>"
+      //get a list of the page ID's to use
+      wikiPage = Array.from(data.query.pageids);
+ console.log(data)
+      //Iterate through the wiki data and if it contains the field add to the new block div, if not not continue to the next field
+      for (var i = 0; i < 9; i++) {
+        $("#addedResults").append(
+          "<div class =' block' id = 'block" + i + "'" + ">" + "</div>" + "<br>"
         );
 
-        //Make stream data an object and check. Color accordinly, red= offline, Green= online.
-        var myObj = JSON.parse(json.addProp.responseText);
-        console.log(kblock);
-        kblock = "#block" + k;
-        if (myObj.stream === null) {
-          $(kblock).css("background-color", "#6441a5");
-          $(kblock).addClass("offline");
-        } else {
-          $(kblock).css("background-color", "#82a541");
-          $(kblock).addClass("online");
+        var result = "#block" + i;
+        var text = "#" + result + "text";
+        var image = "#" + result + "image";
+
+        $(result).append(
+          "<h1 class='title'>" + data["query"]["pages"][wikiPage[i]]["title"] + "</h1>"
+        );
+
+        if (data["query"]["pages"][wikiPage[i]]["description"] != undefined)
+          $(result).append(
+            "<p class='description'>" + data["query"]["pages"][wikiPage[i]]["description"] + "</p>"
+          );
+
+        if (data["query"]["pages"][wikiPage[i]]["extract"] != undefined) {
+          $(result).append(
+              data["query"]["pages"][wikiPage[i]]["extract"] 
+          );
         }
-        // Keep track of the channel ID for manipulation.
-        k++;
-        kblock = "#block" + k;
 
-        page.push(data.url);
-      })
-    };
-  });
-
-  //Open stream once channel is clicked
-  $("#content").on("click", ".block", function() {
-    urlLink = page[this.id.toString().substring(5)];
-    //window.open(urlLink)
-    var a = "#" + this.id;
-   $('#linked-page').empty();
-    twitch.channel = urlLink.substring(22);
-    lastChannel = twitch.channel;
-     if(window.innerWidth <600){
- twitch.width = window.innerWidth ;
-   twitch.height= window.innerHeight  ;
-}else{
-    twitch.width = window.innerWidth * 0.77;
-    twitch.height= window.innerHeight * 0.77 ;
-}
-    openTwitch();
-    
-    $(".block").css("margin-left", "3px");
-    $(a).css("margin-left", "25px");
-  });
-
-  //When online buttons are clicked show the corresponding channels and color the button to show the current status
-  $("#online").click(function() {
-    $("#off").css("text-shadow", "none");
-    $("#bo").css("text-shadow", "none");
-    $(".online").show();
-    $(".offline").hide();
-    
-    $(".block").css("margin-left", "3px");
-    $("#on").css(
-      "text-shadow",
-      " 0 0 20px #82a54#82a541"
-    );
-  });
-
-  $("#offline").click(function() {
-    $("#on").css("text-shadow", "none");
-    $("#bo").css("text-shadow", "none");
-    $(".online").hide();
-    $(".offline").show();
-   
-    $(".block").css("margin-left", "3px");
-    $("#off").css(
-      "text-shadow",
-       " 0 0 20px #82a541, 0 0 20px #82a541"
-    );
-  });
-
-  $("#both").click(function() {
-    $("#off").css("text-shadow", "none");
-    $("#on").css("text-shadow", "none");
-    $(".online").show();
-    $(".offline").show();
-    
-    $(".block").css("margin-left", "3px");
-    $("#bo").css(
-      "text-shadow",
-       " 0 0 20px #82a541, 0 0 20px #82a541"
-    );
-  });
-
-  //Twitch stream link
-  function openTwitch() {
-    new Twitch.Embed("linked-page", twitch);
+        // page.push(wiki + data["query"]["pages"][wikiPage[i]]["title"]);
+      }
+    });
   }
+
+  //When a term is typed, grab value and add it to the url for the API call and call Ajax function.
+  $("#btn").on("click", function() {
+    searchTerm = $("#search").val();
+    url =
+      "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cdescription%7Cextracts&indexpageids=1&pageids=10&generator=search&piprop=thumbnail%7Cname&exsentences=1&exintro=1&excontinue=2&gsrsearch=" +
+      searchTerm +
+      "&callback=?";
+
+    //Hide the form inputs and show the recieved API data.
+    $("#search").val("");
+    ajax();
+   $("#form").hide('slow');
+    $("#results").show('slow');
+    
+    
+  });
+
+  $(".closebutton").on("hover", function() {
+    $(this).css("cursor", "pointer");
+  });
+
+  //On close empty results and show form ready for the next input.
+  $(".closebutton").on("click", function() {
+    $("#form").show();
+    $("#results").hide();
+    $("#addedResults").empty();
+  });
+
+  //If a result is clinked forward me o the wikipedia page.
+  $("#results").on("click", ".block", function() {
+    urlLink = page[this.id.toString().substring(5)];
+    window.open(urlLink);
+  });
+
+  //When a result is clicked determine its ID
+  $("#container").on("click", ".added_element", function() {
+    var id = $(this).attr("id");
+    alert(id);
+  });
+
+  //If random button is clicked open a random wiki article.
+  $("#randombtn").on("click", function() {
+    window.open("https://en.wikipedia.org/wiki/Special:Random");
+  });
+
+
+$('#randombtn').hover(function(){
+  $('#randombtn').css('background-color','red')
+  $('.fa-random').css('color','#e2e2e2')
+},function(){
+  $('#randombtn').css('background-color','#ec7063')
+  $('.fa-random').css('color','white')
+})
+
+$('#btn').hover(function(){
+  $('#btn').css('background-color','#010101')
+  $('.fa-arrow-circle-right').css('color','#e2e2e2')
   
-  $('#logo').click(function(){
-    window.open('https://www.twitch.tv')
-  })
+}, function(){
+  $('#btn').css('background-color','#494949')
+  $('.fa-arrow-circle-right').css('color','white')
+})
+
+
+
+$(window).resize(function(){
+ if($(window).width()<800){
+  console.log('should be done')
+  $('i').removeClass('fa-2x');
+ }else if($(window).width()>800){
+  console.log('should be done')
+  $('i').addClass('fa-2x');
+ }
+
+
+
+});
 });
